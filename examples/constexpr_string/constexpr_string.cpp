e348c2a8b4ee61a8345660ad4b95b20ed5815d60
wandvvs <maximkrotov123@mail.ru>
2025-01-15 21:29:19 +0300

    add: raw ver of constexpr_string

---
+++ examples/constexpr_string/constexpr_string.cpp
@@ -0,0 +1,20 @@
+#include <qutil/constexpr_string.hpp>
+
+auto main() -> int {
+  constexpr auto str1 = qutil::make_constexpr_string("Hello");
+  constexpr qutil::constexpr_string str2(" world");
+  constexpr qutil::constexpr_string<4> str3(" by");
+
+  constexpr auto t = str1 + str2 + str3;
+
+  static_assert(t.size() == 14);
+
+  constexpr auto sv = qutil::make_constexpr_string("ababa");
+  static_assert(sv == "ababa");
+
+  constexpr std::string_view sv2("ababa");
+  static_assert(sv2 == sv);
+
+  auto st = sv.to_string();
+  static_assert(std::is_same_v<std::string, decltype(st)>);
+}
---
+++ include/qutil/constexpr_string.hpp
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <ostream>
+
+namespace qutil {
+template <std::size_t Size>
+class constexpr_string {
+ public:
+  explicit constexpr_string() = default;
+  constexpr explicit constexpr_string(std::string_view str) {
+    std::ranges::copy_n(str.begin(), str.size() > this->size() ? str.size() : this->size(), str_.begin());
+  }
+
+  constexpr constexpr_string(const char (&str)[Size]) {  // NOLINT
+    std::ranges::copy_n(str, Size, this->str_.begin());
+  };
+
+  constexpr explicit operator std::string_view() const& {
+    return {this->begin()};
+  }
+
+  constexpr auto operator==(std::string_view other) const -> bool {
+    return static_cast<std::string_view>(*this) == other;
+  }
+
+  constexpr auto operator==(const char* other) const -> bool {
+    return static_cast<std::string_view>(*this) == other;
+  }
+
+  constexpr auto operator==(const constexpr_string<Size>& other) -> bool {
+    return static_cast<std::string_view>(*this) == static_cast<std::string_view>(other);
+  }
+
+  template <std::size_t Ssize>
+  constexpr auto operator==(const constexpr_string<Ssize>& other) -> bool {
+    return false;
+  }
+
+  constexpr auto operator==(const std::string& other) {
+    return static_cast<std::string_view>(*this) == other;
+  }
+  template <std::size_t Ssize>
+  constexpr auto operator+(const constexpr_string<Ssize>& other) const -> const constexpr_string<Size + Ssize - 1> {
+    constexpr_string<Size + Ssize - 1> res;
+    std::ranges::copy_n(this->begin(), Size - 1, res.begin());
+    std::ranges::copy_n(other.begin(), Ssize, res.begin() + Size - 1);
+
+    return res;
+  }
+
+  [[nodiscard]] constexpr auto to_string() const -> std::string {
+    return {str_.data()};
+  }
+
+  constexpr auto size() const {
+    return Size == 0 ? 0 : Size - 1;
+  }
+
+  [[nodiscard]] constexpr auto begin() const -> const char* {
+    return str_.begin();
+  }
+
+  constexpr auto begin() -> char* {
+    return str_.begin();
+  }
+
+  [[nodiscard]] constexpr auto end() const -> const char* {
+    return str_.end();
+  }
+
+  constexpr auto end() -> char* {
+    return str_.end();
+  }
+
+ private:
+  std::array<char, Size> str_{};
+};
+
+template <std::size_t Size>
+constexpr constexpr_string<Size> make_constexpr_string(const char (&str)[Size]) {  // NOLINT
+  return qutil::constexpr_string<Size>(str);
+}
+
+}  // namespace qutil
+
+template <std::size_t N>
+constexpr auto operator<<(std::ostream& os, const qutil::constexpr_string<N>& str) -> std::ostream& {
+  os << static_cast<std::string_view>(str);
+  return os;
+}
