e24377e819aa925b2a7a55a143df6d49aff17f0c
wandvvs <maximkrotov123@mail.ru>
2025-01-18 20:32:30 +0300

    add: raw ver of result

---
+++ examples/result/result.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <qutil/result.hpp>
+
+struct division_error {
+  enum class error_kind { division_by_zero };
+
+  explicit division_error(error_kind kind, std::string reason = "") : kind(kind), reason(std::move(reason)) {
+  }
+
+  error_kind kind;
+  std::string reason;
+};
+
+auto division(float x, float y) -> result<float, division_error> {
+  if(y == 0) {
+    return err(division_error(division_error::error_kind::division_by_zero, "Division by zero"));
+  }
+  return ok(x / y);
+}
+
+auto main() -> int {
+  auto div = division(5, 0);  // NOLINT
+
+  if(div.is_okay()) {
+    std::cout << div.ok_value() << std::endl;
+  } else if(div.is_err()) {
+    std::cout << div.err_value().reason;
+  }
+}
---
+++ include/qutil/result.hpp
@@ -0,0 +1,79 @@
+#include <utility>
+#include <variant>
+
+template <class OkT, class ErrT>
+struct result;
+
+template <class T>
+struct ok {
+  T value_;
+
+  explicit constexpr ok(const T& value) : value_(value) {
+  }
+
+  explicit constexpr ok(T&& value) : value_(std::move(value)) {
+  }
+
+  [[nodiscard]] constexpr auto value(this auto&& self) -> decltype(auto) {
+    return std::forward_like<decltype(self)>(self.value_);
+  }
+
+  template <class Err>
+  constexpr operator result<T, Err>() const {
+    return result<T, Err>(ok(value_));
+  }
+};
+
+template <class T>
+struct err {
+  T value_;
+
+  explicit constexpr err(const T& value) : value_(value) {
+  }
+
+  explicit constexpr err(T&& value) : value_(std::move(value)) {
+  }
+
+  [[nodiscard]] constexpr auto value(this auto&& self) -> decltype(auto) {
+    return std::forward_like<decltype(self)>(self.value_);
+  }
+
+  template <class Ok>
+  constexpr operator result<Ok, T>() const {
+    return result<Ok, T>(err(value_));
+  }
+};
+
+template <class OkT, class ErrT>
+struct result {
+  std::variant<ok<OkT>, err<ErrT>> variant_;
+
+  constexpr result(ok<OkT> value) : variant_(std::move(value)) {
+  }
+  constexpr result(err<ErrT> value) : variant_(std::move(value)) {
+  }
+
+  [[nodiscard]] constexpr auto is_okay() const -> bool {
+    return std::holds_alternative<ok<OkT>>(variant_);
+  }
+
+  [[nodiscard]] constexpr auto is_err() const -> bool {
+    return std::holds_alternative<err<ErrT>>(variant_);
+  }
+
+  [[nodiscard]] constexpr auto ok_value() const -> OkT {
+    return std::get<ok<OkT>>(variant_).value_;
+  }
+
+  [[nodiscard]] constexpr auto err_value() const -> ErrT {
+    return std::get<err<ErrT>>(variant_).value_;
+  }
+
+  constexpr auto take_ok_value() -> OkT&& {
+    return std::get<ok<OkT>>(variant_).take_value();
+  }
+
+  constexpr auto take_err_value() -> ErrT&& {
+    return std::get<err<ErrT>>(variant_).take_value();
+  }
+};
